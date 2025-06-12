ddff7805d5479ba2a206e80338f96a94ac9c5ad9
Nightingaale <shirounightingaleoff@gmail.com>
2025-06-13 01:33:52 +0300

    feature: add jwt decoder & web filter

--- src/main/java/org/nightingaale/gateway/config/SecurityConfig.java
+++ src/main/java/org/nightingaale/gateway/config/SecurityConfig.java
@@ -1,4 +1,32 @@
 package org.nightingaale.gateway.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("http://auth-service:8090/realms/auth-service/protocol/openid-connect/certs")
+                .build();
+    }
+
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/v1/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                )
+                .build();
+    }
 }

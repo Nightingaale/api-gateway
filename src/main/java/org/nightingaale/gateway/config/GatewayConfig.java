2748baade00c7f9975372a0073f57bc62009049d
Nightingaale <shirounightingaleoff@gmail.com>
2025-06-14 21:17:18 +0300

    feature: add rest template for http requests

--- src/main/java/org/nightingaale/gateway/config/GatewayConfig.java
+++ src/main/java/org/nightingaale/gateway/config/GatewayConfig.java
@@ -1,4 +1,14 @@
 package org.nightingaale.gateway.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }

07b1816d78d20c75bfdc5d06c202812542ac6b4d
Nightingaale <shirounightingaleoff@gmail.com>
2025-06-17 01:32:19 +0300

    feature: update gateway config

--- src/main/java/org/nightingaale/gateway/config/GatewayConfig.java
+++ src/main/java/org/nightingaale/gateway/config/GatewayConfig.java
@@ -1,8 +1,12 @@
 package org.nightingaale.gateway.config;
-
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 public class GatewayConfig {
@@ -10,5 +14,31 @@ public class GatewayConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+                // Swagger
+                .route("auth_service_docs", r -> r.path("/aggregate/auth-service/v1/openapi-docs")
+                        .filters(f -> f.rewritePath("/aggregate/auth-service/v1/openapi-docs", "/v1/openapi-docs"))
+                        .uri("lb://auth-service"))
+
+                // Main path
+                .route("auth_service", r -> r.path("/api/v1/auth/**")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("authServiceCircuitBreaker")
+                                .setFallbackUri("forward:/fallbackRoute")))
+                        .uri("lb://auth-service"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackHandler() {
+        return RouterFunctions.route()
+                .GET("/fallback", request -> ServerResponse
+                        .status(org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE)
+                        .bodyValue("[Service now unavailable. Try again later.]"))
+                .build();
     }
 }

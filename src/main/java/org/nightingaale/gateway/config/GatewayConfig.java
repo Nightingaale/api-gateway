7fd3bbef852e8c68cf03e17c55f22a148148255b
Nightingaale <shirounightingaleoff@gmail.com>
2025-06-22 03:00:51 +0300

    refactor: update comments

--- src/main/java/org/nightingaale/gateway/config/GatewayConfig.java
+++ src/main/java/org/nightingaale/gateway/config/GatewayConfig.java
@@ -20,12 +20,12 @@ public class GatewayConfig {
     public RouteLocator customRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
 
-                // Swagger
+                // Swagger(example)
                 .route("auth_service_docs", r -> r.path("/aggregate/auth-service/v1/openapi-docs")
                         .filters(f -> f.rewritePath("/aggregate/auth-service/v1/openapi-docs", "/v1/openapi-docs"))
                         .uri("lb://auth-service"))
 
-                // Main path
+                // Main path(example)
                 .route("auth_service", r -> r.path("/api/v1/auth/**")
                         .filters(f -> f.circuitBreaker(c -> c.setName("authServiceCircuitBreaker")
                                 .setFallbackUri("forward:/fallbackRoute")))

f2e9bc49c0dcabf0e959b7cf33867a6748079531
Nightingaale <shirounightingaleoff@gmail.com>
2025-07-22 01:27:25 +0300

    feature: add user-service route

--- src/main/java/org/nightingaale/gateway/config/GatewayConfig.java
+++ src/main/java/org/nightingaale/gateway/config/GatewayConfig.java
@@ -5,8 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class GatewayConfig {
@@ -30,12 +31,18 @@ public class GatewayConfig {
                         .filters(f -> f.circuitBreaker(c -> c.setName("authServiceCircuitBreaker")
                                 .setFallbackUri("forward:/fallbackRoute")))
                         .uri("lb://auth-service"))
+
+                .route("user_service", r -> r.path("/api/v1/users/**")
+                .filters(f -> f.circuitBreaker(c -> c.setName("userServiceCircuitBreaker")
+                        .setFallbackUri("forward:/fallbackRoute")))
+                .uri("lb://user-service"))
+
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> fallbackHandler() {
-        return RouterFunctions.route()
+        return route()
                 .GET("/fallback", request -> ServerResponse
                         .status(org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE)
                         .bodyValue("[Service now unavailable. Try again later.]"))
